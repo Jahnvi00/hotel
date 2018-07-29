6ad5c29ccb4b7a995670a1a0cf42170bb446f969
Jahnvi00 <41137535+Jahnvi00@users.noreply.github.com>
2018-07-29 18:37:23 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,7 @@
 <?php
 include('config.php');
-$delete_record = $_GET['del'];
-$query ="DELETE FROM dbo.jahnvi WHERE id='$delete_record'";
+$delete_record=$_GET['del'];
+$query="DELETE FROM dbo.jahnvi WHERE id='$delete_record'";
 $getResult=sqlsrv_query($conn, $query);
 echo "<script>";
         echo "window.alert('Data Deleted')
