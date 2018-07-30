674f016797757375d32ffe4d3f58f8a6f87b8b18
Jahnvi00 <41137535+Jahnvi00@users.noreply.github.com>
2018-07-30 10:06:12 +0530

    Update view.php

--- view.php
+++ view.php
@@ -51,8 +51,8 @@
 				<td><?php echo $f_Name; ?></td>
 				<td><?php echo $f_country; ?></td>
 				<td><?php echo $f_phone; ?></td>
-					<td><?php echo $f_adress; ?></td>
-				<td><a href ='delete.php?del=<?php echo $adress ?>'>Delete</a></td>
+				<td><?php echo $f_adress; ?></td>
+				<td><a href='delete.php?del=<?php echo $adress ?>'>Delete</a></td>
 				<td><a href='edit.php?edit=<?php echo $u_id ?>'>Edit</a></td>
 				<td><a href='view.php?detail=<?php echo $u_id ?>'>Detail</a></td>
 				
