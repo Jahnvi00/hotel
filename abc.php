f2aca99206eff09568643e719de30637b5955dc2
Jahnvi00 <41137535+Jahnvi00@users.noreply.github.com>
2018-07-30 10:27:57 +0530

    Update abc.php

--- abc.php
+++ abc.php
@@ -49,14 +49,14 @@ include('config.php');
  
  if(isset($_POST['submit']))
  {
- $food_Name=$_POST['Name'];
- $food_country=$_POST['country'];
- $food_phone=$_POST['phone'];
- $food_adress=$_POST['adress'];
+ $Name=$_POST['Name'];
+ $country=$_POST['country'];
+ $phone=$_POST['phone'];
+ $adress=$_POST['adress'];
  
  
  
-	 $que="insert into dbo.jahnvi(Name,country,phone,adress)values('$food_Name','$food_country','$food_phone','$food_adress')";
+	 $que="insert into dbo.jahnvi(Name,country,phone,adress)values('$Name','$country','$phone','$adress')";
 $getResults= sqlsrv_query($conn, $que);
 	 
 
@@ -64,7 +64,7 @@ $getResults= sqlsrv_query($conn, $que);
 //if($conn->query($que)=== true)
 //{
 	echo "<center><b>The follwing Data has been inserted into our databse:</b></center>";
-	echo "<table width='500px'align='center' border='4'><tr><td>$food_Name</td><td>$food_country</td><td>$food_phone</td><td>$food_adress</td></tr></table>";                      
+	echo "<table width='500px'align='center' border='4'><tr><td>$Name</td><td>$country</td><td>$phone</td><td>$adress</td></tr></table>";                      
 //}
 
 
