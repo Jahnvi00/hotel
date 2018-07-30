187cc343d61b39e6f0278f871f669fa0d46a2bd5
Jahnvi00 <41137535+Jahnvi00@users.noreply.github.com>
2018-07-30 09:55:00 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -7,10 +7,4 @@ echo "<script>";
         echo "window.alert('Data Deleted')
 	window.location.href='view.php';
 </script>";
-/*
-//if($getResult===true ){
-if($conn->query($que)=== true){
-	
-	//echo "<script>window.open('view.php?delete=Record Has been Deleted Successfully!','_self')<script>";
-}
-//}*/
+?>
