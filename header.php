bc1186d1e9f8e9464f0c5ae53b42b92c28cb1e63
Jahnvi00 <41137535+Jahnvi00@users.noreply.github.com>
2018-07-31 16:24:48 +0530

    Update header.php

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 			<li><a href="#">Register</a></li>
 			<li><a href="#">Contact Us</a></li>
 			 <li>
-							<a href="view.php">View</a>
+							<a href="view.php">View the Record</a>
 </li>
 			
 		</ul>
