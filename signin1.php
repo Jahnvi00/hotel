f819cab82f9cf24cc1bb23baecc0a444c91a543f
Jahnvi00 <41137535+Jahnvi00@users.noreply.github.com>
2018-08-02 13:55:48 +0530

    Update signin1.php

--- signin1.php
+++ signin1.php
@@ -7,7 +7,7 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Login Form </title>
-	<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 </head>
 <body>
 	<div class="container">
